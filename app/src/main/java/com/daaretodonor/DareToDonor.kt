ec4274bd591e9021375d0038d62d13f4cc919c35
mochamadramdhan <mochamadramdhan241200@gmail.com>
2023-12-16 09:38:27 +0700

    update backroud navigation bar and add shadow

--- app/src/main/java/com/daaretodonor/DareToDonor.kt
+++ app/src/main/java/com/daaretodonor/DareToDonor.kt
@@ -1,12 +1,19 @@
 package com.daaretodonor
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -70,55 +77,69 @@ private fun BottomBar(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
-    NavigationBar(
-        modifier = modifier,
-    ) {
-        val navigationItems = listOf(
-            NavigationItem(
-                title = stringResource(R.string.menu_home),
-                iconResourceId =  R.drawable.ic_home,
-                screen = Screen.Home
-            ),
-            NavigationItem(
-                title = stringResource(R.string.menu_news),
-                iconResourceId = R.drawable.ic_news,
-                screen = Screen.Profile
-            ),
-            NavigationItem(
-                title = stringResource(R.string.menu_riwayat),
-                iconResourceId =  R.drawable.ic_clock,
-                screen = Screen.News
-            ),
-            NavigationItem(
-                title = stringResource(R.string.menu_profil),
-                iconResourceId =  R.drawable.ic_user,
-                screen = Screen.Profile
-            ),
-        )
-        navigationItems.map { item ->
-            val icon = painterResource(id = item.iconResourceId!!)
-            NavigationBarItem(
-                icon = {
-                    Image(
-                        painter = icon,
-                        contentDescription = item.title,
-                        modifier = Modifier.size(24.dp)
-                    )
-                },
-                label = { Text(item.title) },
-                selected = false,
-                onClick = {
-                   navController.navigate(item.screen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
-                        restoreState = true
-                        launchSingleTop = true
-                    }
-                }
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(topStart = 0.dp, topEnd = 0.dp) // Adjust the radius as needed
             )
-        }
+            .shadow(3.dp)
+    ) {
+        NavigationBar(
+            containerColor = Color.White,
+            contentColor = Color.Black,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 7.dp)
+        ) {
+            val navigationItems = listOf(
+                NavigationItem(
+                    title = stringResource(R.string.menu_home),
+                    iconResourceId = R.drawable.ic_home,
+                    screen = Screen.Home
+                ),
+                NavigationItem(
+                    title = stringResource(R.string.menu_news),
+                    iconResourceId = R.drawable.ic_news,
+                    screen = Screen.Profile
+                ),
+                NavigationItem(
+                    title = stringResource(R.string.menu_riwayat),
+                    iconResourceId = R.drawable.ic_clock,
+                    screen = Screen.News
+                ),
+                NavigationItem(
+                    title = stringResource(R.string.menu_profil),
+                    iconResourceId = R.drawable.ic_user,
+                    screen = Screen.Profile
+                ),
+            )
+            navigationItems.map { item ->
+                val icon = painterResource(id = item.iconResourceId!!)
+                NavigationBarItem(
+                    icon = {
+                        Image(
+                            painter = icon,
+                            contentDescription = item.title,
+                            modifier = Modifier.size(24.dp)
+                        )
+                    },
+                    label = { Text(item.title) },
+                    selected = false,
+                    onClick = {
+                        navController.navigate(item.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
 
+        }
     }
 }
 
--- app/src/main/java/com/daaretodonor/ui/screen/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/daaretodonor/ui/screen/welcome/WelcomeScreen.kt
@@ -7,14 +7,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,7 +23,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.daaretodonor.R
@@ -77,7 +74,7 @@ fun WelcomeScreen(modifier: Modifier = Modifier) {
                     text = stringResource(R.string.decription_welcome),
                     style = MaterialTheme.typography.bodyLarge,
                     fontWeight = FontWeight.Bold,
-                    color = Color.White ,
+                    color = Color.White,
                     modifier = Modifier
                         .fillMaxWidth(0.8f)
                 )
